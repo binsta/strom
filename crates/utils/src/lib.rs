c7c7e4163138bc384bf042547661c8ffd2c3a867
binsta <binstoncardoza@gmail.com>
2023-08-06 10:25:32 +0530

    add128 test has done

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -1,5 +1,5 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-use alloc::vec::{self, Vec};
+use std::vec::{self, Vec};
 
 pub fn uint_vec<T>(len: usize) -> Vec<T> {
     let mut vec = Vec::with_capacity(len);
