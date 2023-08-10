af4f689bd503ac8afe892562c723dbddc214974e
binsta <binstoncardoza@gmail.com>
2023-08-10 19:42:08 +0530

    - Polynom has added by on filled_vec()

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -1,8 +1,14 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-use std::vec::{self, Vec};
+use std::vec::Vec;
 
 pub fn uint_vec<T>(len: usize) -> Vec<T> {
     let mut vec = Vec::with_capacity(len);
     unsafe { vec.set_len(len) };
     return vec;
+}
+
+pub fn filled_vec<T: Copy>(length: usize, capacity: usize, value: T) -> Vec<T> {
+    let mut vector = vec![value; capacity];
+    vector.truncate(length);
+    return vector;
 }
